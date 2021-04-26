1a09192274dbc5109cd785d0fc0d129bebd97a22
Max Huckstepp <mhuckstepp@gmail.com>
2021-04-26 08:35:00 -0700

    first commit

---
+++ api/data/migrations/20210124181032_first-migration.js
@@ -0,0 +1,12 @@
+exports.up = async (knex) => {
+  await knex.schema.createTable("users", (users) => {
+    users.increments("user_id");
+    users.string("user_email", 320).notNullable();
+    users.string("user_password", 200).notNullable();
+    users.timestamps(false, true);
+  });
+};
+
+exports.down = async (knex) => {
+  await knex.schema.dropTableIfExists("users");
+};
