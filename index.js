dc0074c85070ca9b2b622628928d8d36406e885c
Max Huckstepp <mhuckstepp@gmail.com>
2021-04-29 17:07:15 -0700

    added jwt secret

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const path = require('path')
-const express = require('express')
+const server = require("./api/server");
 
-const server = require('./api/server')
+const port = process.env.PORT;
 
-const port = process.env.PORT
+// server.use(express.static(path.join(__dirname, 'client/dist')))
 
-server.use(express.static(path.join(__dirname, 'client/dist')))
+// server.get('*', (req, res) => {
+//   // if you want to serve a SPA using Express you totally can!
+//   res.sendFile(path.join(__dirname, 'client/dist', 'index.html'))
+// })
 
-server.get('*', (req, res) => {
-  // if you want to serve a SPA using Express you totally can!
-  res.sendFile(path.join(__dirname, 'client/dist', 'index.html'))
-})
+server.use("/", (req, res) => {
+  res.send({ message: "Welcome to our water my plants api" });
+});
 
 server.listen(port, () => {
-  console.log('listening on ' + port)
-})
+  console.log("listening on " + port);
+});
