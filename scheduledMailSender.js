1459ada3059a0a9646f9187294b97e46c5cc5417
Max Huckstepp <mhuckstepp@gmail.com>
2021-06-27 18:56:30 -0700

    .env issues

--- scheduledMailSender.js
+++ scheduledMailSender.js
@@ -3,24 +3,21 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const db = require("./api/data/db-config");
 
 const mailSender = async (recip, content) => {
-    const msg = {
-        to: recip,
-        from: "mhuckstepp@gmail.com",
-        subject: content.subject,
-        text: content.text,
-        html: `<p>${content.text}</p>`,
-      };
-      sgMail
-        .send(msg)
-        .then(() => {
-          console.log("Email sent");
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+  const msg = {
+    to: recip,
+    from: "mhuckstepp@gmail.com",
+    subject: content.subject,
+    text: content.text,
+    html: `<p>${content.text}</p>`,
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email sent");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 };
-
-const 
-
 
 mailSender();
