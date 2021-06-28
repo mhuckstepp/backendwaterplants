d14dcfe69c59a1b8cc7c06685b7f7556b99ee449
Max Huckstepp <mhuckstepp@gmail.com>
2021-06-27 19:09:31 -0700

    working on scheduled mailer

--- scheduledMailSender.js
+++ scheduledMailSender.js
@@ -2,8 +2,13 @@ const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const db = require("./api/data/db-config");
 
+const recipt = "mhuckstepp@gmail.com";
+const conter = {
+  subject: "HELLOOO",
+  text: " nice to e-meet you",
+};
+
 const mailSender = async (recip, content) => {
-<<<<<<< HEAD
   const msg = {
     to: recip,
     from: "mhuckstepp@gmail.com",
@@ -21,26 +26,4 @@ const mailSender = async (recip, content) => {
     });
 };
 
-=======
-    const msg = {
-        to: recip,
-        from: "mhuckstepp@gmail.com",
-        subject: content.subject,
-        text: content.text,
-        html: `<p>${content.text}</p>`,
-      };
-      sgMail
-        .send(msg)
-        .then(() => {
-          console.log("Email sent");
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-};
-
-const 
-
-
->>>>>>> 116c0761fc17aa5d8ad8395ff03ddd69b15071ca
-mailSender();
+mailSender(recipt, conter);
